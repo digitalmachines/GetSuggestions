1bf8d8b1062b39cbde0e86bd4c9fa8b41438892d
John Daly <jcdaly97@gmail.com>
2020-04-27 09:36:54 -0700

    added empty files

---
+++ post-here-front-end/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="Landing Page">
+      <h4>navbar</h4>
+      <h1>Title</h1>
+      <p>description of app</p>
+    </div>
+  );
+}
+
+export default App;
