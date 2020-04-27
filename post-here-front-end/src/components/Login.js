a0e4ca931dacbb2740224b4e3977cb128a56e62d
matthew-heideman <heidemanmr@yahoo.com>
2020-04-27 17:11:01 -0500

    Completed Skeleton For Login Form

---
+++ post-here-front-end/src/components/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Form, FormGroup, FormText, Button } from 'reactstrap';
+
+function LoginForm(){
+    return(
+        <>
+            <Form>
+                <FormGroup>
+                    <FormText>Email:</FormText>
+                    <label htmlFor = 'email'>
+                        <input type = 'email' name = 'email' id ='email' />
+                    </label>
+                </FormGroup>
+                <FormGroup>
+                    <FormText>Password:</FormText>
+                    <label htmlFor = 'password'>
+                        <input type = 'password' name = 'password' id = 'password' />
+                    </label>
+                </FormGroup>
+                <FormGroup>
+                    <Button color = 'danger'>
+                        Submit
+                    </Button>
+                </FormGroup>
+            </Form>
+        </>
+    )
+} 
+export default LoginForm;
