1cf7d8eb7d293c5bda5df6c55e05943912471b8d
John Daly <jcdaly97@gmail.com>
2020-04-28 12:25:37 -0700

    register and login are working

--- post-here-front-end/src/components/Login.js
+++ post-here-front-end/src/components/Login.js
@@ -1,28 +1,82 @@
 import React from 'react';
 import { Form, FormGroup, FormText, Button } from 'reactstrap';
+import {useState} from 'react'
+import { useHistory } from "react-router-dom";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+import axios from 'axios'
+
 
 function LoginForm(){
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+      })
+    const history = useHistory()
+
+    const handleChange = e=>{
+        e.preventDefault()
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+      }
+
+    const login = e=>{
+        e.preventDefault()
+        axiosWithAuth()
+        .post('/login', credentials)
+        .then(res=>{
+          console.log(res, 'retrieved the token')
+          localStorage.setItem('token', res.data.token)
+          history.push(`/protected/${res.data.id}`)
+        })
+        .catch(res=>{
+          console.log(res)
+          alert('the username or password is incorrect')
+        })
+      }
+
+    const sendToRegister =()=>{
+        history.push('/register')
+    }
+
     return(
         <>
-            <Form>
+            <Form onSubmit = {login}>
+
                 <FormGroup>
                     <FormText>Email:</FormText>
                     <label htmlFor = 'email'>
-                        <input type = 'email' name = 'email' id ='email' />
+                        <input
+                             type="text"
+                             name="username"
+                             value={credentials.username}
+                             onChange={handleChange}
+                        />
                     </label>
                 </FormGroup>
+
                 <FormGroup>
                     <FormText>Password:</FormText>
                     <label htmlFor = 'password'>
-                        <input type = 'password' name = 'password' id = 'password' />
+                        <input 
+                            type="password"
+                            name="password"
+                            value={credentials.password}
+                            onChange={handleChange}
+                        />
                     </label>
                 </FormGroup>
+
                 <FormGroup>
-                    <Button color = 'danger'>
+                    <Button color = 'danger' type='submit'>
                         Submit
                     </Button>
                 </FormGroup>
+
             </Form>
+            <p>don't have an account?</p>
+            <Button onClick={sendToRegister}>Click here to make one!</Button>
         </>
     )
 } 
