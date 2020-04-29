ffa8ce04ba8b7bd4b8dcce3f101ad4dcfaa386a1
j-greenberg <jgreenberg@runbox.com>
2020-04-29 13:27:34 -0400

    created Header component

--- post-here-front-end/src/components/Header.js
+++ post-here-front-end/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'; 
-
+import { Link } from 'react-router-dom'; 
 
 function Header(props){
     return(
@@ -8,7 +8,19 @@ function Header(props){
                 <h1 className="title">
                     <Link to="/">PostHere for Reddit</Link>
                 </h1>
+                <div>
+                    { user ? (
+                        <Link to="/logout">Logout</Link>
+                    ) : (
+                        <> 
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Sign Up</Link>
+                        </>
+                    )}
+                </div>
             </nav>
         </header>
     ); 
 }; 
+
+export default Header; 
