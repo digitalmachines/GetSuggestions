396bbed0841c8c8e4c74ff4f50db2a948f807ff3
j-greenberg <jgreenberg@runbox.com>
2020-04-30 13:13:43 -0400

    fixed key issue

--- src/components/PastPosts.js
+++ src/components/PastPosts.js
@@ -6,7 +6,7 @@ const PastPosts = (props)=>{
     return(
         <ul>
         {props.posts.map(item => (
-          <PastPost deletePost = {props.handleDelete} userId ={props.userId} title = {item.post_title} text = {item.post_text} postId = {item.id}/>  
+          <PastPost deletePost = {props.handleDelete} userId ={props.userId} title = {item.post_title} text = {item.post_text} postId = {item.id} key={item.id}/>  
         ))}
       </ul>
     )
